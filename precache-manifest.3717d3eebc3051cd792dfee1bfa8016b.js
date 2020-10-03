18d0b39965980ec86c3f98e68881aecc4b4fb20b
moblaze360 <victorvalenti@yahoo.co.uk>
2020-10-03 15:40:56 +0200

    Updates

---
+++ precache-manifest.3717d3eebc3051cd792dfee1bfa8016b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "574acda48cfba8aae6f8900bb262e3de",
+    "url": "/coolbots/index.html"
+  },
+  {
+    "revision": "82144ecc970dc7f09142",
+    "url": "/coolbots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "223e5e620fceac1ff014",
+    "url": "/coolbots/static/css/main.6b52be78.chunk.css"
+  },
+  {
+    "revision": "82144ecc970dc7f09142",
+    "url": "/coolbots/static/js/2.d398c6c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coolbots/static/js/2.d398c6c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223e5e620fceac1ff014",
+    "url": "/coolbots/static/js/main.155b0734.chunk.js"
+  },
+  {
+    "revision": "57588ff0de8099aa055e",
+    "url": "/coolbots/static/js/runtime-main.60618dbb.js"
+  },
+  {
+    "revision": "814db3fcbb4a092cf9ea63fabe00f8ce",
+    "url": "/coolbots/static/media/NewSuperman-Pzd7.814db3fc.woff"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/coolbots/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0172c651ecde833ba460713268daeeae",
+    "url": "/coolbots/static/media/batmfa__.0172c651.woff"
+  },
+  {
+    "revision": "e6e10fdf66cf9f4f84d2b269586cf040",
+    "url": "/coolbots/static/media/batmfo__.e6e10fdf.woff"
+  }
+]);
